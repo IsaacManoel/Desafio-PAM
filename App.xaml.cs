c16ee70f8419d3ecb74244385dd24dbe87638b84
IsaacManoel <162491159+IsaacManoel@users.noreply.github.com>
2024-09-25 15:26:23 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace desafio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
